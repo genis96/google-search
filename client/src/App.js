8a436f758eb160a9065a7b1528b58879613c971d
Genis Bardales <genis96@yahoo.com>
2021-04-11 18:45:27 -0400

    App.js inside client

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
-import Books from "./pages/Books";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Nav from "./components/Nav";
-import Detail from "./pages/Detail";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 
+import "./App.css";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+class App extends Component {
+ 
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
 
 
 export default App;
