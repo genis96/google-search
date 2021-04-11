5c71126d970fd35b89e8e4137f30544bdb62bb97
Genis Bardales <genis96@yahoo.com>
2021-04-11 18:57:56 -0400

    saved js file inside pages

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,4 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import API from "../utils/API";
+import Results from "../components/Results";
 
+class Saved extends Component {
+    state = {
+        savedBooks: [],
+    }
 
+    componentDidMount() {
+        API.savedBooks()
+        .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+        .catch(err => console.error(err));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h2>Saved Books</h2>
+                <Results books={this.state.savedBooks}></Results>
+            </div>
+        )
+    }
+}
+
+export default Saved;
