f50237a5f1b6d10ae8fbcaadcb13b8a1091895e2
Genis Bardales <genis96@yahoo.com>
2021-04-11 19:00:00 -0400

    search js

---
+++ client/src/pages/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import API from "../utils/API";
+import Form from "../components/Form";
+import Results from "../components/Results";
+
+class Search extends React.Component {
+    state = {
+        books: [],
+        value: ""
+    };
+}
+
+export default Search;
