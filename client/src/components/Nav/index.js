e8c1fd0695d38a4be7e22ea17c2f635eb8822ca7
Genis Bardales <genis96@yahoo.com>
2021-04-11 19:57:57 -0400

    working on nav

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Nav/style.css";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <div>
+                <Link className="navbar-brand" to="/"></Link>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link
+                        className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+                        to="/search"
+                        />
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
+
