d9ab0dd91ce4211bfb9a30f0de59eb8a968976cd
Genis Bardales <genis96@yahoo.com>
2021-04-11 18:22:58 -0400

    files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
 import Detail from "./pages/Detail";
