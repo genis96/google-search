cf224f5b0e7a62846cbf249d18031c0d90aee3f2
Genis Bardales <genis96@yahoo.com>
2021-04-11 21:22:05 -0400

    it displays books but my navbar does not display the search and saved

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -28,32 +28,33 @@ class Results extends Component {
         return (
             <div>
                 {!this.props.books.length ? (
-                    <h1 className="text-center"> No Results </h1>
+                    <h1 className="text-center">No Results</h1>
                 ) : (
-                    <div>
-                        { this.props.map(result => (
-                            <div className="card mb-4" key={ result._id }>
-                                <div className="row">
-                                    <div className="col-md-2">
-                                        <img className="img-fluid" alt={ result.title } src={ result.image } />
-                                    </div>
-                                    <div className="col-md-10">
-                                        <div className="card-body">
-                                            <h4 className="card-title">{result.title} by: {result.authors}</h4>
-                                            <p className="card-text">{result.description}</p>
-                                            <div>
-                                                <a target="_blank" className="btn badge-pill btn-outline-dark mt-2" href={ result.link }>Links</a>
-                                                <button onClick={() => this.handleSave(result)} className="btn badge-pill btn-outline-primary ml-3 mt-3">
-                                                    { this.state.savedBooks.map(book => book._id).includes(result._id) ? "Unsave" : "Save" }
-                                                </button>
+                        <div>
+                            {this.props.books.map(result => (
+                                <div className="card mb-3" key={result._id}>
+                                    <div className="row">
+                                        <div className="col-md-2">
+                                            <img src={result.image} alt={result.title} className="img-fluid" />
+                                        </div>
+                                        <div className="col-md-10">
+                                            <div className="card-body">
+                                                <h5 className="card-title">{result.title} by {result.authors}</h5>
+                                                <p className="card-text">{result.description}</p>
+                                                <div>
+                                                    <a className="btn  btn-outline-dark mt-3" href={result.link} target="_blank" >Views</a>
+  
+                                                    <button onClick={() => this.handleSave(result)} className="btn btn-outline-primary mt-3 ml-3" >
+                                                        {this.state.savedBooks.map(book => book._id).includes(result._id) ? "Unsave" : "Save"}
+                                                    </button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div> 
-                        )) }
-                    </div>
-                )}
+                            ))}
+                        </div>
+                    )}
             </div>
         )
     }
