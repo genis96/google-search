4a14fefb4008860c289e3a53c242b685bf018495
Genis Bardales <genis96@yahoo.com>
2021-04-11 20:21:23 -0400

    results component

---
+++ client/src/components/Results/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+class Results extends Component {
+    state = {
+        savedBooks: [],
+    }
+
+    componentDidMount() {
+        API.savedBooks()
+        .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+        .catch(err => console.error(err));
+    }
+}
+
+export default Results;
