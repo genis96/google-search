30b82f7c15f96d83475bb8c3e369e5c2f4be4c6b
Genis Bardales <genis96@yahoo.com>
2021-04-11 20:16:46 -0400

    form ok

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 function Form(props) {
-    
+    return (
+        <div className="container">
+            <form>
+                <div className="form-group">
+                    <label htmlFor="search">
+                        <h2>Search & Save</h2>
+                    </label>
+
+                    <input 
+                    id="search"
+                    name="search"
+                    placeholder="Search"
+                    onChange={props.handleInputChange}
+                    value={props.search}
+                    className="form-control"
+                    type="text"
+                    />
+                    <button onClick={props.handleFormSubmit} className="btn btn-dark mt-2 mb-4">
+                        Search
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default Form;
