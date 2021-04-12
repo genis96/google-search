86512bc888e75c47faf6729d0741f466cf77ceb1
Genis Bardales <genis96@yahoo.com>
2021-04-11 20:06:36 -0400

    header is o

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "../Header/style.css";
+
+function Header() {
+    return (
+        <div className="jumbotron text-center">
+            <div className="container">
+                <h1>Google API Search Book</h1>
+            </div>
+        </div>
+    );
+}
+
+
+export default Header;
