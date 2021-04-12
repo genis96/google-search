7d1157177441034988b37cfff31683015ee63824
Genis Bardales <genis96@yahoo.com>
2021-04-11 20:40:28 -0400

    results section JSX

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -11,6 +11,33 @@ class Results extends Component {
         .then(savedBooks => this.setState({ savedBooks: savedBooks }))
         .catch(err => console.error(err));
     }
+
+    render() {
+        return (
+            <div>
+                {!this.props.books.length ? (
+                    <h1 className="text-center"> No Results </h1>
+                ) : (
+                    <div>
+                        { this.props.map(result => (
+                            <div className="card mb-4" key={ result._id }>
+                                <div className="row">
+                                    <div className="col-md-2">
+                                        <img className="img-fluid" alt={ result.title } src={ result.image } />
+                                    </div>
+                                    <div className="col-md-10">
+                                        <div className="card-body">
+                                            <h4 className="card-title">{result.title} by: {result.authors}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )) }
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
 
 export default Results;
